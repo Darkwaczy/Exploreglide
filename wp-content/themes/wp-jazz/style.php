8b2a7d8b6998cb44a8613c774e5837a682ae8bbe
Darkwaczy <115374946+Darkwaczy@users.noreply.github.com>
2023-08-01 15:21:38 -0700

    Latest

---
+++ wp-content/themes/wp-jazz/style.php
@@ -0,0 +1,208 @@
+/* --------------[ User-Defined Adjustments from Stylesheet #2 ]-------------- */
+
+@import 'style-2.css';
+
+/* --------------[ Color Scheme Adjustgments ]-------------- */
+
+
+/* ------[ Style Adjustments for Light Blue Color Scheme ]-------- */
+
+body {
+background-image: url(images/ltblue/body.gif);
+background-color:#ccc;
+}
+
+#header {
+background-color:#006699;
+<!-- background-image: url(images/ltblue/head-bg.gif); -->
+border-bottom:1px solid #CCEEFF;
+}
+
+#head-content {
+<!-- background-image: url(images/ltblue/logo2.gif); -->
+}
+
+a, a:link, a:visited {
+color:#4D809A;
+}
+
+a:hover, a:active {
+color:#000000;
+}
+
+
+/* --------------[ User-Defined Adjustments ]-------------- */
+
+body {
+background-color: #4D809A;
+background-image:none;
+font-size: 9pt;
+font-family: arial,helvetica,sans-serif;
+color: #333333;
+}
+
+
+h1, h2, h3, h4, h5, h6, h7, .sitetitle {
+font-family: arial,helvetica,sans-serif;
+font-weight: normal;
+}
+
+/* -------------------[ Site Title Adjustments ]------------------- */
+
+#sitetitle h1, #sitetitle .title {
+font-size: 10px;
+color: #ffffff;
+text-align: Left;
+font-weight: normal;
+font-family: arial,helvetica,sans-serif;
+}
+
+#sitetitle .description {
+color:#ffffff;
+text-align:Left;
+}
+
+
+#sitetitle {
+float:none;
+text-indent:-10000em;
+position:absolute;
+display:none;
+}
+
+#sitetitle {
+width:980px;
+}
+
+
+
+/* ----------[ Header Background Color Adjustments ]---------- */
+
+#header {
+background-color: #ffffff;
+background-image:none;
+}
+
+/* ----------[ Left Sidebar Float Adjustments ]----------- */
+
+#content .col-3 { float:right; }
+#sidebarleft { float:left; }
+
+/* --------------[ Top Navigation Adjustments ]-------------- */
+
+#topnav {
+background: #3c6c92;
+font-size: 9pt;
+font-weight: bold;
+}
+
+#topnav li a, #topnav li a:link, #topnav li a:visited {
+color: #eeeeee;
+}
+
+#topnav li a:hover, #topnav li a:active {
+color: #ffffff;
+background: #FF9C01;
+}
+
+#topnav li ul {
+background: #3c6c92;
+}
+
+/* --------------[ Category Navigation Adjustments ]-------------- */
+
+#nav {
+background: #ffffff;
+font-size: 9pt;
+font-weight: bold;
+}
+
+#nav li a, #nav li a:link, #nav li a:visited {
+color: #3c6c92;
+}
+
+#nav li a:hover, #nav li a:active {
+color: #ffffff;
+background: #FF9C01;
+}
+
+#nav li ul {
+background: #ffffff;
+}
+
+/* --------------[ Features Adjustments ]-------------- */
+
+#my-glider {
+background-color: #ffffff;
+background-image: none;
+background-repeat: no-repeat;
+background-position: 0 0;
+color: #333333;
+font-size: 10pt;
+}
+
+
+#my-glider a, #my-glider a:link, #my-glider a:visited, #my-glider .controls li.feat-head {
+color: #003366;
+}
+
+#my-glider a:hover, #my-glider a:active, #my-glider .controls a.active {
+color:#CC6600 !important;
+}
+
+
+/* --------------[ Main Content Adjustments ]-------------- */
+
+.maincontent, maincontent2, maincontentnew {
+font-size: 10pt;
+}
+
+.maincontent a, .maincontent a:link, .maincontent a:visited {
+color: #003366;
+}
+
+.maincontent a:hover, .maincontent a:active, .post h1 a:active, .post h1 a:hover, .post h2 a:active, .post h2 a:hover {
+color: #CC6600;
+}
+
+
+/* --------------[ Sidebar-Left Adjustments ]-------------- */
+
+#sidebarleft {
+font-size: 8pt;
+}
+
+#sidebarleft a, #sidebar-left a:link, #sidebar-left a:visited {
+color: #3c6c92;
+}
+
+#sidebarleft a:hover, #sidebar-left a:active {
+color: #CC6600;
+}
+
+/* --------------[ Sidebar-Right Adjustments ]-------------- */
+
+#sidebar {
+font-size: 9pt;
+}
+
+#sidebar a, #sidebar a:link, #sidebar a:visited {
+color: #3c6c92;
+}
+
+#sidebar a:hover, #sidebar a:active {
+color: #CC6600;
+}
+
+/* --------------[ Footer Adjustments ]-------------- */
+
+#footer {
+font-size:8pt;
+color:#000000;
+}
+
+#footer a, #footer a:link, #footer a:visited {
+}
+
+#footer a:hover, #footer a:active {
+}
